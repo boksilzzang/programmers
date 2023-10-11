32acdae1d25fb58fbf0896b89f2c89f40b846d28
Eunbyeol <124585629+boksilzzang@users.noreply.github.com>
2023-10-11 23:51:31 +0900

    [level unrated] Title: ad 제거하기, Time: 1.03 ms, Memory: 87.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181870. ad 제거하기/ad 제거하기.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Solution {
+    public String[] solution(String[] strArr) {
+        List<String> answer = new ArrayList<>();
+        for (String str : strArr) {
+            if (!str.contains("ad")) {
+                answer.add(str);
+            }
+        }
+        return answer.toArray(new String[answer.size()]);
+    }
+}
